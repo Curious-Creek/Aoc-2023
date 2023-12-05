02453886f0dc6cc4223b39356ee357f8120c4281
Max Beekmans <max.beekmans@hotmail.com>
2023-12-05 21:43:08 +0100

    AOC day three

---
+++ Aoc.2023.3.1/Program.cs
@@ -0,0 +1,105 @@
+﻿const string InputPath = "./daythree_input.txt";
+var input = File.ReadAllLines(InputPath);
+
+List<int> partNumbers = new();
+
+for (int y = 0; y < input.Length; y++)
+{
+    string partNumber = string.Empty;
+    int firstDigit = -1;
+    int lastDigit = -1;
+    
+    for (int x = 0; x < input[y].ToCharArray().Length; x++)
+    {
+        if (char.IsDigit(input[y][x]))
+        {
+            if (firstDigit == -1)
+                firstDigit = x;
+            
+            partNumber += input[y][x];
+            
+            if (x == input[y].ToCharArray().Length - 1)
+            {
+                lastDigit = x;
+            }
+        } else if (partNumber != string.Empty)
+        {
+            lastDigit = x - 1;
+        }
+        
+        if (firstDigit != -1 && lastDigit != -1)
+        {
+            int leftBorder = firstDigit - 1;
+            int rightBorder = lastDigit + 1;
+
+            if (leftBorder < 0)
+            {
+                leftBorder = 0;
+            } else
+            {
+                // Check left
+                if (IsNotDigitOrDot(input[y][leftBorder]))
+                {
+                    partNumbers.Add(int.Parse(partNumber));
+                    partNumber = string.Empty;
+                    firstDigit = -1;
+                    lastDigit = -1;
+                    continue;
+                }
+            }
+
+            if (rightBorder > input[y].Length - 1)
+            {
+                rightBorder = input[y].Length - 1;
+            } else
+            {
+                // Check right
+                if (IsNotDigitOrDot(input[y][rightBorder]))
+                {
+                    partNumbers.Add(int.Parse(partNumber));
+                    partNumber = string.Empty;
+                    firstDigit = -1;
+                    lastDigit = -1;
+                    continue;
+                }
+            }
+
+            for (int i = leftBorder; i <= rightBorder; i++)
+            {
+                // Check upper row
+                if (y != 0)
+                {
+                    if (IsNotDigitOrDot(input[y - 1][i]))
+                    {
+                        partNumbers.Add(int.Parse(partNumber));
+                        break;
+                    }
+                }
+
+                // Check lower row
+                if (y != input.Length - 1)
+                {
+                    if (IsNotDigitOrDot(input[y + 1][i]))
+                    {
+                        partNumbers.Add(int.Parse(partNumber));
+                        break;
+                    }
+                }
+            }
+
+            partNumber = string.Empty;
+            firstDigit = -1;
+            lastDigit = -1;
+        }
+    }
+}
+
+var result = partNumbers.Sum();
+Console.WriteLine(result);
+
+return 1;
+
+bool IsNotDigitOrDot(char c)
+{
+    return !char.IsDigit(c) && c != '.';
+}
---
+++ Aoc.2023.3.2/Program.cs
@@ -0,0 +1,135 @@
+﻿const string InputPath = "./daythree_input.txt";
+var input = File.ReadAllLines(InputPath);
+
+List<int> gearRatios = new();
+
+for (int i = 0; i < input.Length; i++)
+{
+    for (int j = 0; j < input[i].Length; j++)
+    {
+        if (input[i][j] == '*')
+        {
+            var adjacentNumbers = AdjacentNumbers(j - 1, j + 1, i);
+
+            if (adjacentNumbers.Length > 1)
+            {
+                gearRatios.Add(adjacentNumbers[0] * adjacentNumbers[1]);
+            }
+        }
+    }
+}
+
+var result = gearRatios.Sum();
+Console.WriteLine(result);
+
+return 1;
+
+int[] AdjacentNumbers(int left, int right, int y)
+{
+    List<int> numbers = new();
+    var (leftBorder, rightBorder) = CheckXBorder(left, right, y);
+    
+    // Check left
+    if (char.IsDigit(input[y][leftBorder]))
+    {
+        numbers.Add(AssemblePartNumber(leftBorder, y));
+    }
+    
+    // Check right
+    if (char.IsDigit(input[y][rightBorder]))
+    {
+        numbers.Add(AssemblePartNumber(rightBorder, y));
+    }
+
+    numbers.AddRange(GetNumbersFromOtherY(y - 1, leftBorder, rightBorder));
+    numbers.AddRange(GetNumbersFromOtherY(y + 1, leftBorder, rightBorder));
+    
+    return numbers.ToArray();
+}
+
+int[] GetNumbersFromOtherY(int y, int leftBorder, int rightBorder)
+{
+    if (y >= input.Length || y < 0)
+    {
+        return Array.Empty<int>();
+    }
+
+    var hasDigitLeftCorner = char.IsDigit(input[y][leftBorder]);
+    var hasDigitRightCorner = char.IsDigit(input[y][rightBorder]);
+    
+    // Check if both corners have a digit with a . in between
+    // This is the case where there are multiple numbers in the same row
+    if (hasDigitLeftCorner && hasDigitRightCorner)
+    {
+        if (!char.IsDigit(input[y][leftBorder + 1]))
+        {
+            int leftNumber = AssemblePartNumber(leftBorder, y);
+            int rightNumber = AssemblePartNumber(rightBorder, y);
+            return new[] { leftNumber, rightNumber };
+        }
+
+        return new[] { AssemblePartNumber(leftBorder, y) };
+    }
+
+    // If only left corner has a digit, return that number
+    if (hasDigitLeftCorner)
+    {
+        return new[] { AssemblePartNumber(leftBorder, y) };
+    }
+
+    // If only right corner has a digit, return that number
+    if (hasDigitRightCorner)
+    {
+        return new[] { AssemblePartNumber(rightBorder, y) };
+    }
+
+    return Array.Empty<int>();
+}
+
+Tuple<int, int> CheckXBorder(int left, int right, int y)
+{
+    if (left < 0)
+    {
+        left = 0;
+    }
+    
+    if (right > input[y].Length - 1)
+    {
+        right = input[y].Length - 1;
+    }
+
+    return new Tuple<int, int>(left, right);
+}
+
+int AssemblePartNumber(int x, int y)
+{
+    string partNumber = input[y][x].ToString();
+    
+    //Check to left of coords
+    for (int i = x - 1; i >= 0; i--)
+    {
+        if (char.IsDigit(input[y][i]))
+        {
+            partNumber = input[y][i] + partNumber;
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    //Check to right of coords
+    for (int i = x + 1; i < input[y].Length; i++)
+    {
+        if (char.IsDigit(input[y][i]))
+        {
+            partNumber += input[y][i];
+        }
+        else
+        {
+            break;
+        }
+    }
+
+    return int.Parse(partNumber);
+}
