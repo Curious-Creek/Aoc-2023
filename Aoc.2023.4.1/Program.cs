55cf231b667754d7d3bd050202329f8e5cd742ad
Max Beekmans <max.beekmans@hotmail.com>
2023-12-16 14:25:12 +0100

    AOC day four

---
+++ Aoc.2023.4.1/Program.cs
@@ -0,0 +1,37 @@
+﻿const string InputPath = "./dayfour_input.txt";
+var input = File.ReadAllLines(InputPath);
+
+var pointsSum = 0;
+
+foreach (var line in input)
+{
+    var inputNumbers = line.Substring(line.IndexOf(":") + 2);
+    var split = inputNumbers.Split(" | ");
+    var cardNumbers = split[0].Split(" ").Where(x => x != "").Select(int.Parse);
+    var winningNumbers = split[1].Split(" ").Where(x => x != "").Select(int.Parse);
+
+    var points = 0;
+
+    foreach (var cardNumber in cardNumbers)
+    {
+        if (winningNumbers.Contains(cardNumber))
+        {
+            if (points == 0)
+            {
+                points = 1;
+            }
+            else
+            {
+                points *= 2;
+            }
+        }
+    }
+
+    pointsSum += points;
+}
+
+Console.WriteLine(pointsSum);
+return 1;
+
+
+
---
+++ Aoc.2023.4.2/Program.cs
@@ -0,0 +1,21 @@
+﻿const string InputPath = "./dayfour_input.txt";
+var input = File.ReadAllLines(InputPath);
+
+int[] numberOfCards = new int[input.Length];
+
+for (int i = 0; i < input.Length; i++)
+{
+    var inputNumbers = input[i].Substring(input[i].IndexOf(":") + 2);
+    var split = inputNumbers.Split(" | ");
+    var cardNumbers = split[0].Split(" ").Where(x => x != "").Select(int.Parse).ToArray();
+    var winningNumbers = split[1].Split(" ").Where(x => x != "").Select(int.Parse);
+    numberOfCards[i] = 1;
+    
+    for (int j = 0; j < cardNumbers.Length; j++)
+    {
+        if (winningNumbers.Contains(cardNumbers[i]))
+        {
+            numberOfCards[i + 1]++;
+        }
+    }
+}
